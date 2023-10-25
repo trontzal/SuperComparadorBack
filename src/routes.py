e4c3c46e32cafd4fb276833a13566e5e7acc5eac
Trontzal <trontzal@gmail.com>
2023-10-25 15:28:58 +0200

    usando la api correcta ya sale foto y todo

--- src/routes.py
+++ src/routes.py
@@ -32,3 +32,14 @@ def get_mercadona_productos_en_categoria(id):
         return jsonify(data)
     else:
         return jsonify({'error': 'Error en la solicitud'}), 500
+    
+@app.route('/productosEspecificos/mercadona/<int:id>')
+def get_mercadona_productos_especificos(id):
+    url = f'https://tienda.mercadona.es/api/products/{id}'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        return jsonify(data)
+    else:
+        return jsonify({'error': 'Error en la solicitud'}), 500
